7f98a908b5ee48773894ffea6560cde96389342e
suika7150 <weft71500@gmail.com>
2026-04-29 18:56:10 +0800

    刪減不必要的 file 、

--- src/main/java/com/gjun/ecs/enums/ProductStatus.java
+++ src/main/java/com/gjun/ecs/enums/ProductStatus.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum ProductStutes {
+public enum ProductStatus {
   ONSALE("2", "銷售中"),
   STOPED("1", "停售"),
   DELETE("0", "刪除");
@@ -15,9 +15,9 @@ public enum ProductStutes {
 
   // 取得產品狀態描述
   public static String getDesc(String code) {
-    for (ProductStutes productStutes : ProductStutes.values()) {
-      if (productStutes.getCode().equals(code)) {
-        return productStutes.getDesc();
+    for (ProductStatus productStatus : ProductStatus.values()) {
+      if (productStatus.getCode().equals(code)) {
+        return productStatus.getDesc();
       }
     }
     return code;
