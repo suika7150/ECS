06bedbad020d9b87e138b076822424cfc3998c96
suika7150 <weft71500@gmail.com>
2026-07-13 13:51:00 +0800

    test : 整合並重整 Controller 重複宣告

--- src/main/java/com/shop/ecs/controller/OrderController.java
+++ src/main/java/com/shop/ecs/controller/OrderController.java
@@ -1,16 +1,13 @@
 package com.shop.ecs.controller;
 
 import com.shop.ecs.dto.request.OrderReq;
-import com.shop.ecs.dto.response.OrderResp;
 import com.shop.ecs.dto.response.Outbound;
 import com.shop.ecs.enums.OrderStatus;
 import com.shop.ecs.service.OrderService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,24 +25,21 @@ public class OrderController {
   @Autowired
   private OrderService orderService;
 
-  @PostMapping(path = "/orders", consumes = MediaType.APPLICATION_JSON_VALUE)
+  @PostMapping("/orders")
   @Operation(summary = "建立訂單")
   public ResponseEntity<Outbound> createOrder(@RequestBody OrderReq orderReq) {
-    OrderResp resp = orderService.createOrder(orderReq);
-    return ResponseEntity.ok(Outbound.ok(resp));
+    return ResponseEntity.ok(Outbound.ok(orderService.createOrder(orderReq)));
   }
 
   @GetMapping("/orders")
   @Operation(summary = "獲取目前登入使用者的訂單列表")
   public ResponseEntity<Outbound> getOrderList(@RequestParam(required = false) OrderStatus status) {
-    List<OrderResp> orders = orderService.getUserOrders(status);
-    return ResponseEntity.ok(Outbound.ok(orders));
+    return ResponseEntity.ok(Outbound.ok(orderService.getUserOrders(status)));
   }
 
   @GetMapping("/orders/{orderId}")
   @Operation(summary = "獲取訂單詳情")
   public ResponseEntity<Outbound> getOrderDetail(@PathVariable String orderId) {
-    OrderResp order = orderService.getOrderDetail(orderId);
-    return ResponseEntity.ok(Outbound.ok(order));
+    return ResponseEntity.ok(Outbound.ok(orderService.getOrderDetail(orderId)));
   }
 }
