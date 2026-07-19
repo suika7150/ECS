7aef7a32726d847905bee66050c29e88e7d0f8e9
suika7150 <weft71500@gmail.com>
2026-07-19 16:41:23 +0800

    refactor: 優化全域異常處理與 Exception 類別結構

--- src/main/java/com/shop/ecs/exception/ApplicationException.java
+++ src/main/java/com/shop/ecs/exception/ApplicationException.java
@@ -2,18 +2,19 @@ package com.shop.ecs.exception;
 
 import com.shop.ecs.constant.ResultCode;
 
-import lombok.Data;
+import lombok.Getter;
 
-@Data
-// @EqualsAndHashCode(callSuper = true)
+@Getter
 public class ApplicationException extends Exception {
 
-  private String code;
-  private String msg;
+	private static final long serialVersionUID = 1L;
 
-  public ApplicationException(ResultCode resultCode) {
-    super(resultCode.getMsg());
-    this.code = resultCode.getCode();
-    this.msg = resultCode.getMsg();
-  }
+	private final String code;
+	private final String msg;
+
+	public ApplicationException(ResultCode resultCode) {
+		super(resultCode.getMsg());
+		this.code = resultCode.getCode();
+		this.msg = resultCode.getMsg();
+	}
 }
